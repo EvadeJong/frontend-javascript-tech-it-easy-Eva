4fecee6d8443ad9d27d7c4acb3b8d9c2cee31efa
EvadeJong <eefjuh.de.jong@gmail.com>
2022-04-10 14:37:34 +0200

    opdracht 4 + bonus af

--- opdracht4.js
+++ opdracht4.js
@@ -62,3 +62,132 @@ console.log(letsDoItAll(inventory[4]));
 
 inventory.map((tvItem) =>  console.log(letsDoItAll(tvItem)));
 
+//Bonus opdracht
+//1. Maak drie knoppen op de pagina: `Sorteer op prijs`, `AmbiLight TV's` en `Uitverkochte exemplaren`. Gebruik de code
+//die je in opdracht 1b, 1c en 1d hebt gemaakt en schrijf dit om naar functies zodat je ze kunt aanroepen op het moment
+//dat de buttons geklikt worden. Zorg ervoor dat de functies de uitkomsten in de console loggen als de gebruiker op
+//de bijbehorende knop klikt. _Tip_: lees hiervoor paragraaf 7.4 op EdHub eens door!
+//maak knoppen
+//schrijf functies om de code in opdracht 1b, c en d
+//koppel deze functies aan de knoppen
+//zorg dat bij een button click antwoorden gelogt worden
+
+
+function findSoldOut(){
+    let soldOutList = inventory.filter((tvObject)=> {
+        if(tvObject.originalStock - tvObject.sold === 0){
+            return tvObject;
+        }
+
+    });
+    console.log(soldOutList);
+}
+
+
+function findAmbilight(){
+    const ambiLight = inventory.filter((tvObject)=>{
+        return tvObject.options.ambiLight === true;
+    });
+    console.log(ambiLight);
+}
+
+function sortPrice(){
+    const sorted = inventory.sort((a,b) =>{
+        return a.price - b.price;
+    });
+    console.log(sorted);
+}
+
+
+
+//2. Zorg er nu voor, in plaats van dat de uitkomsten in de console worden gelogd, dat de uitkomsten worden meegegeven aan
+//jouw `generateTV`-functie zodat de resultaten daadwerkelijk op de pagina weergegeven worden!
+const listID = 'button-generated-list';
+let title = '';
+function createULElementWithId(){
+    const unorderedList = document.createElement('ul');
+    unorderedList.setAttribute('id', listID);
+    document.body.appendChild(unorderedList);
+    return unorderedList;
+}
+function createTitle(){
+    const title = document.createElement('h3');
+    title.setAttribute('id', 'title');
+    document.body.appendChild(title);
+    return title;
+}
+
+function generateSoldOutList(){
+    //definieer de id die je op de ul wil zetten
+
+    //kijk of er al een button lijst bestaat,
+    let soldOutList = document.getElementById(listID);
+    if(soldOutList === null) {
+        //zo niet roep createTitle aam voor een nieuwe titel
+        title = createTitle();
+        //en roep de createULElementWithId aan om de lijst te creeren
+        soldOutList = createULElementWithId();
+    } else {
+        //verwijder het eerste child element, zodat er geen childeren meer zijn
+        //anders wordt de lijst telkens opnieuw toegevoegd aan het scherm
+        title.textContent = '';
+        while(soldOutList.firstChild) {
+            soldOutList.removeChild(soldOutList.firstChild);
+        }
+    }
+    title.textContent = 'Onze uitverkochte televisies:';
+    //filter de array op elementen die geen voorraad meer hebben en voeg deze toe aan de nieuwe array
+    const soldOutTvs = inventory.filter((tvObject)=> {
+        if(tvObject.originalStock - tvObject.sold === 0){
+            return tvObject;
+        }
+    });
+    //maak voor ieder item in de lijst een listitem aan
+    soldOutTvs.forEach((soldOutTv) => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `${soldOutTv.brand} - ${soldOutTv.name}`;
+        soldOutList.appendChild(listItem);
+    });
+}
+
+function generateAmbiLightList() {
+    let ambiLightList = document.getElementById(listID);
+    if (ambiLightList === null) {
+        title = createTitle();
+        ambiLightList = createULElementWithId();
+    } else {
+        while (ambiLightList.firstChild) {
+            ambiLightList.removeChild(ambiLightList.firstChild);
+        }
+    }
+    title.textContent = 'Onze ambilight televisies:';
+    const ambiLightTV = inventory.filter((tvObject) => {
+        return tvObject.options.ambiLight === true;
+    });
+    ambiLightTV.forEach((ambiLightTV)=>{
+        const listItem = document.createElement('li');
+        listItem.textContent = ambiLightTV.name;
+        ambiLightList.appendChild(listItem);
+    });
+}
+
+function generateSortedPriceList(){
+    let sortedPriceList = document.getElementById(listID);
+    if (sortedPriceList === null) {
+        title = createTitle();
+        sortedPriceList = createULElementWithId();
+    }else{
+        while (sortedPriceList.firstElementChild){
+        sortedPriceList.removeChild(sortedPriceList.firstElementChild);
+        }
+    }
+    title.textContent = 'Onze televisies op prijs gesorteerd:'
+    const sortedPriceItem = inventory.sort((a,b) =>{
+        return a.price - b.price;
+    });
+    sortedPriceItem.forEach((sortedPriceItem) =>{
+        const listItem = document.createElement('li');
+        listItem.textContent = `${sortedPriceItem.brand} - ${sortedPriceItem.name} kost ${sortedPriceItem.price} euro`;
+        sortedPriceList.appendChild(listItem);
+    })
+}
